9f1294eba8c89af5460e38e20c2598b8f63722dc
Daichama <d.sakai.25@gmail.com>
2026-08-03 11:40:41 +0900

    Delete Daichi_Training_Web_complete directory

--- Daichi_Training_Web_complete/app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'D-log',description:'Daichi Training Log'};
-export default function RootLayout({children}){return <html lang="ja"><body>{children}</body></html>}
